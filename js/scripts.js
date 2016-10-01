d20efff7f9e5e693eda0992c514bd2291d007baa
Karol Poplawski <karolpoplawski@gmail.com>
2016-10-01 16:55:14 +0200

    changed scripts.js's location

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var allNames = [];
+var womensNames = ['Asia', 'Kasia', 'Ola', 'Jola']; 
+var mensNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = allNames.concat(womensNames, mensNames);
+
+console.log(allNames);
+
+var newName ='Marian';
+
+if(allNames.indexOf(newName) == -1){
+	allNames.push(newName);
+}
+
+console.log(allNames);
